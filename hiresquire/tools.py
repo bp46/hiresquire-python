94c01fe81a7f47921af4a4183cefd3876a947827
BeauP <bparrish46@gmail.com>
2026-05-09 12:40:17 +0700

    chore: bump version to 1.2.3 and update repository links

--- hiresquire/tools.py
+++ hiresquire/tools.py
@@ -95,7 +95,7 @@ def _get_headers(token: Optional[str] = None, idempotency_key: Optional[str] = N
     headers = {
         "Authorization": f"Bearer {api_token}",
         "Content-Type": "application/json",
-        "User-Agent": "hiresquire-python/1.2.2"
+        "User-Agent": "HireSquirePythonSDK/1.2.3"
     }
     
     if idempotency_key:
